86ba46328177a0950f380c9d5bc2f0913eefea80
devffery <mails4icy@gmail.com>
2024-06-25 10:07:58 +0100

    home page view

--- src/App.js
+++ src/App.js
@@ -6,18 +6,67 @@ import {
   Route,
 } from "react-router-dom";
 
+import Navbar from "./components/navbar/Navbar";
+import LeftBar from "./components/leftBar/LeftBar";
+import RightBar from "./components/rightBar/RightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import { Navigate, Outlet } from 'react-router-dom';
+
 function App() {
+
+  const currentUser = true;
+
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <div style={{flex:6}}>
+            <Outlet />
+          </div>
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
 
   const router = createBrowserRouter([
     {
-      path:"/login",
-      element:<Login/>,
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        }
+      ]
     },
     {
-      path:"/register",
-      element:<Register/>,
+      path: "/login",
+      element: <Login />,
     },
-    ]);
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
 
   return (
     <div>
---
+++ src/components/rightBar/RightBar.jsx
@@ -0,0 +1,9 @@
+import './rightBar.scss'
+
+const RightBar = () => {
+	return (
+		<div className='rightbar'>Rightbar</div>
+		)
+}
+
+export default RightBar;
