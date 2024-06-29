a28c04228ddef325604e512f2203312a1f511421
devffery <mails4icy@gmail.com>
2024-06-29 09:25:23 +0100

    comment view added

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -5,8 +5,14 @@ import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import Comments from "../comments/Comments";
+
+
 
 const Post = ({post}) => {
+
+	const [commentOpen, setCommentOpen] = useState(false);
 
 	const liked = false;
 
@@ -35,7 +41,7 @@ const Post = ({post}) => {
 						{liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
 						120 Likes
 					</div>
-					<div className="item">
+					<div className="item" onClick={() => setCommentOpen(!commentOpen)}>
             			<TextsmsOutlinedIcon />
             			12 Comments
           			</div>
@@ -44,6 +50,7 @@ const Post = ({post}) => {
             			Share
           			</div>
 				</div>
+				{commentOpen && <Comments/>}
 			</div>
 		</div>	
 		);
